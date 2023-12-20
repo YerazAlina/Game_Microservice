ad6575903aba5114914601b7a764b1fd8fff4eb9
YerazAlina <70819728+YerazAlina@users.noreply.github.com>
2023-12-20 12:24:05 +0100

    Model

--- app/main/model/game.py
+++ app/main/model/game.py
@@ -3,10 +3,10 @@ from .. import db
 class Game(db.Model):
     __tablename__ = 'game'
 
-    #als ik camelcase gebruik neemt ie dat over in de database en krijg ik een error // str.lower() gebruiken? 
+    #als ik game_name gebruik neemt ie dat over in de database en krijg ik een error (zelfde voor gameName)
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     gamename = db.Column(db.String(50), nullable=False)
-    gamedescription = db.Column(db.String(50), nullable=False)
+    gamedescription = db.Column(db.String(100), nullable=False)
     gamelocation = db.Column(db.String(50), nullable=False)
     gamestarttime = db.Column(db.DateTime, nullable=False)
     gamemaster = db.Column(db.String(50), nullable=False)
